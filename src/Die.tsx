a6aa402c653adc378185149265a10917c31a3820
Murabrasa <francesco.barzetta@outlook.com>
2024-01-14 21:09:24 +0100

    fixes

--- src/Die.tsx
+++ src/Die.tsx
@@ -4,25 +4,33 @@ export default function Die(props){
 
 
     function handleClick(){
-        document.getElementById(props.value)?.play()
-        props.setSoundName(props.id)
+        const element = document.getElementById(props.value) as HTMLMediaElement;
+                if (!element) {
+                    throw new Error('Missing element')
+                    }
+                element.play();
+                props.setSoundName(props.id)
     }
 
     useEffect(() => {
         document.addEventListener('keydown', 
         (e: KeyboardEvent) => {
             if (e.key.toUpperCase() == props.value){
-                document.getElementById(props.value)?.play()
+                const element = document.getElementById(props.value) as HTMLMediaElement;
+                if (!element) {
+                    throw new Error('Missing element')
+                    }
+                element.play();
                 props.setSoundName(props.id)
+                }
             }
-        }
         )
     }, [])
 
     return(
         <div>
         <button tabIndex={0} className="drum-pad" id={props.id} value={props.value} onClick={handleClick}>{props.value}
-        <audio id={props.value} className="clip" src={props.src} type="audio/mpeg"></audio>
+        <audio id={props.value} className="clip" src={props.src}></audio>
         </button>
         
         </div>
